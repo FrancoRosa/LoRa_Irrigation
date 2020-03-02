2a5f398fbc1f7775e041a10412d5ed08d4909eee
FrancoRosa <km115.franco@gmail.com>
2020-03-01 20:23:26 -0500

    Added some models, stuck waiting Leonidas responce about all variables to be stored

--- irrigationsite/nodes/models.py
+++ irrigationsite/nodes/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=200)
+    moisture = models.FloatField()
+    battery = models.FloatField()
+    panel = models.FloatField()
+    com_status = models.BooleanField()
+    last_up = models.DateTimeField
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    tx_frec = models.IntegerField()
+    tx_slot = models.IntegerField()
+    rx_window = models.IntegerField()
+
+class SensorData(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    moisture = models.FloatField()
+    battery = models.FloatField()
+    panel = models.FloatField()
+    com_status = models.BooleanField()
+    update = models.DateTimeField()
+
+class Valve(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=200)
+    flow = models.FloatField()
+    battery = models.FloatField()
+    panel = models.FloatField()
+    com_status = models.BooleanField()
+    valve_status = models.BooleanField()
+    tot_flow = models.FloatField()
+    last_up = models.DateTimeField
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    tx_frec = models.IntegerField()
+    tx_slot = models.IntegerField()
+    rx_window = models.IntegerField()
+
+class Valve(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    moisture = models.FloatField()
+    battery = models.FloatField()
+    panel = models.FloatField()
+    status = models.BooleanField()
+    update = models.DateTimeField()
+
