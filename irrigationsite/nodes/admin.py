98b2f508a6b1a6586a6c6430c0c198bbd09f1ce1
FrancoRosa <km115.franco@gmail.com>
2020-03-03 00:20:21 -0500

    Added models and configured list view on admin

--- irrigationsite/nodes/admin.py
+++ irrigationsite/nodes/admin.py
@@ -3,7 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import Sensor, SensorData, Valve, ValveData
 
-admin.site.register(Sensor)
-admin.site.register(SensorData)
-admin.site.register(Valve)
-admin.site.register(ValveData)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'com_status', 
+                    'moisture', 'battery', 
+                    'panel', 'tx_frec', 'tx_slot')
+
+class SensorDataAdmin(admin.ModelAdmin):
+    list_display = ('sensor', 
+                    'moisture', 'battery', 
+                    'panel', 'update')
+
+
+class ValveAdmin(admin.ModelAdmin):
+    list_display = ('name', 'com_status',
+                    'valve_status', 'valve_command',
+                    'flow', 'tot_flow', 'battery',
+                    'panel', 'tx_frec', 'tx_slot')
+    list_editable = ('valve_command', 'valve_command')
+
+class ValveDataAdmin(admin.ModelAdmin):
+    list_display = ('valve', 
+                    'valve_status', 'flow', 
+                    'battery', 
+                    'panel', 'update')
+
+admin.site.register(Sensor, SensorAdmin)
+admin.site.register(SensorData, SensorDataAdmin)
+admin.site.register(Valve, ValveAdmin)
+admin.site.register(ValveData, ValveDataAdmin)
--- irrigationsite/nodes/models.py
+++ irrigationsite/nodes/models.py
@@ -1,48 +1,54 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Sensor(models.Model):
     name = models.CharField(max_length=25)
-    description = models.CharField(max_length=200)
-    moisture = models.FloatField()
-    battery = models.FloatField()
-    panel = models.FloatField()
-    com_status = models.BooleanField()
-    last_up = models.DateTimeField
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    tx_frec = models.IntegerField()
-    tx_slot = models.IntegerField()
-    rx_window = models.IntegerField()
+    description = models.CharField(max_length=200, blank=True)
+    moisture = models.FloatField(default=0)
+    battery = models.FloatField(default=0)
+    panel = models.FloatField(default=0, verbose_name="Solar Panel")
+    com_status = models.BooleanField(default=False, verbose_name = "Conected")
+    last_up = models.DateTimeField(default=now,  verbose_name = "Last Update")
+    latitude = models.FloatField(default=-13.489983)
+    longitude = models.FloatField(default=-72.457463)
+    tx_frec = models.IntegerField(default=60)
+    tx_slot = models.IntegerField(default=5)
+    rx_window = models.IntegerField(default=10)
+    def __str__(self):
+        return self.name
 
 class SensorData(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
-    moisture = models.FloatField()
-    battery = models.FloatField()
-    panel = models.FloatField()
-    update = models.DateTimeField()
+    moisture = models.FloatField(default=0)
+    battery = models.FloatField(default=0)
+    panel = models.FloatField(default=0)
+    update = models.DateTimeField(default=now,  verbose_name = "Last Update")
 
 class Valve(models.Model):
     name = models.CharField(max_length=25)
-    description = models.CharField(max_length=200)
-    flow = models.FloatField()
-    battery = models.FloatField()
-    panel = models.FloatField()
-    com_status = models.BooleanField()
-    valve_status = models.BooleanField()
-    tot_flow = models.FloatField()
-    last_up = models.DateTimeField
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    tx_frec = models.IntegerField()
-    tx_slot = models.IntegerField()
-    rx_window = models.IntegerField()
+    description = models.CharField(max_length=200, blank=True)
+    flow = models.FloatField(default=0)
+    battery = models.FloatField(default=0)
+    panel = models.FloatField(default=0, verbose_name="Solar Panel")
+    com_status = models.BooleanField(default=False, verbose_name="Conected")
+    valve_command = models.BooleanField(default=False, verbose_name="Open Action")
+    valve_status = models.BooleanField(default=False, verbose_name="Open Status")
+    tot_flow = models.FloatField(default=0)
+    last_up = models.DateTimeField(default=now,  verbose_name = "Last Update")
+    latitude = models.FloatField(default=-13.489983)
+    longitude = models.FloatField(default=-72.457463)
+    tx_frec = models.IntegerField(default=60)
+    tx_slot = models.IntegerField(default=5)
+    rx_window = models.IntegerField(default=10)
+    def __str__(self):
+        return self.name
 
 class ValveData(models.Model):
     valve = models.ForeignKey(Valve, on_delete=models.CASCADE)
-    flow = models.FloatField()
-    battery = models.FloatField()
-    panel = models.FloatField()
-    valve_status = models.BooleanField()
-    update = models.DateTimeField()
+    flow = models.FloatField(default=0)
+    battery = models.FloatField(default=0)
+    panel = models.FloatField(default=0)
+    valve_status = models.BooleanField(default=False, verbose_name="Open Status")
+    update = models.DateTimeField(default=now,  verbose_name = "Last Update")
 
