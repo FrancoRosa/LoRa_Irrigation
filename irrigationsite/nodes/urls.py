b8be825875f1fb6994aba39a1165f6114dde11a0
FrancoRosa <km115.franco@gmail.com>
2020-03-16 10:16:52 -0500

    Add sample data on db, change models

--- irrigationsite/nodes/urls.py
+++ irrigationsite/nodes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sensors', views.sensors, name='sensors'),
+    path('valves', views.valves, name='valves'),
 ]
--- irrigationsite/nodes/views.py
+++ irrigationsite/nodes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.core import serializers
+from .models import Sensor, Valve
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Nodes index.")
+
+def sensors(request):
+    data = serializers.serialize("json", Sensor.objects.all())
+    return HttpResponse(data)
+
+def valves(request):
+    data = serializers.serialize("json", Valve.objects.all())
+    return HttpResponse(data)
